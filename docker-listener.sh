031057ee25865ada283cddee642555b2a4f5baba
Anton Tyutin <anton@tyutin.ru>
2020-06-10 11:57:10 +0300

    Реакция только на сервисы и только в тех же сетях

--- docker-listener.sh
+++ docker-listener.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 REPEAT_INTERVAL=7 # seconds
+AVAILABLE_NETWORKS="$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.NetworkID}}|{{end}}' $(hostname) | sed 's/|$//')"
+
+docker info 2>/dev/null | grep 'Is Manager: true' >/dev/null || ( echo 'This container must be scheduled at the Swarm Manager node'; exit 1 )
 
 exec 2>&1
 
@@ -23,38 +26,50 @@ function parse_tasks {
 }
 
 function rebuild {
-  # non service task's containers
-  (
-    FORMAT='{{range $i, $var := .Config.Env}}{{if $i}}{{"\n"}}{{end}}{{$var}}{{end}}'
-    docker ps -f "is-task=false" -f "status=running" --format '{{.ID}} {{.Names}}' | while read -r ID CONTAINER_NAME
-    do
-      docker inspect --format "$FORMAT" "$ID" | grep -v '^CRON_TASK_' | (
-        import_env
-        docker inspect --format "$FORMAT" "$ID" | grep '^CRON_TASK_' | parse_tasks "container $CONTAINER_NAME"
-      )
-    done
-  )
-  # service task's containers
-  docker info 2>/dev/null | grep 'Is Manager: true' >/dev/null && (
-    FORMAT='{{range $i, $item := .Spec.TaskTemplate.ContainerSpec.Env}}{{if $i}}{{"\n"}}{{end}}{{$item}}{{end}}'
-    docker service ls --format '{{.Name}} {{.Replicas}}' | grep -v '/0 ' | while read -r SERVICE_NAME REPLICAS
-    do
-      docker service inspect --format="$FORMAT" "$SERVICE_NAME" | grep -v '^CRON_TASK_' | (
-        import_env
-        docker service inspect --format="$FORMAT" "$SERVICE_NAME" | grep '^CRON_TASK_' | parse_tasks "service $SERVICE_NAME"
-      )
-    done
-  )
+  FORMAT='{{range $i, $item := .Spec.TaskTemplate.ContainerSpec.Env}}{{if $i}}{{"\n"}}{{end}}{{$item}}{{end}}'
+  docker service ls --format '{{.Name}} {{.Replicas}}' | grep -v '/0 ' | while read -r SERVICE_NAME REPLICAS
+  do
+    docker service inspect --format='{{range .Spec.TaskTemplate.Networks}}{{.Target}}{{"\n"}}{{end}}' "$SERVICE_NAME" | grep -E "^($AVAILABLE_NETWORKS)$" >/dev/null \
+      || continue
+    docker service inspect --format="$FORMAT" "$SERVICE_NAME" | grep -v '^CRON_TASK_' | (
+      import_env
+      docker service inspect --format="$FORMAT" "$SERVICE_NAME" | grep '^CRON_TASK_' | parse_tasks "service $SERVICE_NAME"
+    )
+  done
+}
+
+function check_services_networks {
+  while IFS=$(echo -e "\t") read -r ACTION SERVICE_NAME
+  do
+    if [ "$ACTION" != "remove " ]; then
+      docker service inspect --format='{{range .Spec.TaskTemplate.Networks}}{{.Target}}{{"\n"}}{{end}}' "$SERVICE_NAME" | grep -E "^($AVAILABLE_NETWORKS)$" >/dev/null \
+        && return 0
+    else
+      crontab -l | grep "^# commands of $SERVICE_NAME" >/dev/null \
+        && return 0
+    fi
+  done
+  return 1
+}
+
+function check_need_update {
+  if [ -z "$1" ]; then
+    echo "crontab update on cron service start"
+    return 0
+  else
+    echo "$1" | check_services_networks \
+      && echo "crontab update on $(echo "$1" | sed ':a;$!N;s/\n/, /;ta;s/\t/ /')" \
+      && return 0
+  fi
+  return 1
 }
 
 function update {
-  echo 'crontab update on' "$@"
-  
-  rebuild | crontab -
+  check_need_update "$1" && rebuild | crontab -
 }
 
 RECENTLY_UPDATED_AT="$(date -u "+%Y-%m-%dT%H:%M:%SZ")"
-update cron service start
+update
 
 while true
 do
@@ -62,19 +77,11 @@ do
   EVENTS=$(
     docker events \
         --filter "type=service" \
-        --format "{{.Action}} {{index .Actor.Attributes \"updatestate.new\"}} {{ .Actor.Attributes.name }}" \
+        --format '{{.Action}} {{index .Actor.Attributes "updatestate.new"}}{{"\t"}}{{ .Actor.Attributes.name }}' \
         --since "$RECENTLY_UPDATED_AT" \
         --until "$NOW" \
-      | grep -E '^(remove |create |update completed) '
-    HAS_SERVICES_UPDATED=$?
-    docker events --filter="type=container" \
-        --format "{{if index .Actor.Attributes \"com.docker.swarm.task.id\"}}?{{end}}{{.Status}} {{ .Actor.Attributes.name }}" \
-        --since "$RECENTLY_UPDATED_AT" \
-        --until "$NOW" \
-      | grep -E '^(start|die) '
-    HAS_CONTAINERS_UPDATED=$?
-    [ $HAS_CONTAINERS_UPDATED -eq 0 -o $HAS_SERVICES_UPDATED -eq 0 ]
-  ) && update $(echo "$EVENTS" | sed ':a;$!N;s/\n/, /;ta')
+      | tee /dev/stderr | grep -E '^(remove |create |update completed)'
+  ) && update "$EVENTS"
   RECENTLY_UPDATED_AT="$NOW"
   sleep "$REPEAT_INTERVAL"
 done
